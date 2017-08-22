21b75f173f5a7a9c49333175c3c69be9e8c01679
cthuff <cthuff03@gmail.com>
2017-08-21 20:56:41 -0700

    Initial Commit

---
+++ NCAA-XC-TableTests/NCAA_XC_TableTests.swift
@@ -0,0 +1,36 @@
+//
+//  NCAA_XC_TableTests.swift
+//  NCAA-XC-TableTests
+//
+//  Created by Craig on 8/21/17.
+//  Copyright © 2017 Craig. All rights reserved.
+//
+
+import XCTest
+@testable import NCAA_XC_Table
+
+class NCAA_XC_TableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
